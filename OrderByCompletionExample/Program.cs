403b574479b7b5ab0ea512dc284200a663238d89
ILepekhov <lepvanek@gmail.com>
2021-07-12 10:43:23 +0500

    Добавлен пример с кодом упорядочивания Task'ов в порядке их выполнения.

---
+++ OrderByCompletionExample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderByCompletionExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await PrintDelayedRandomTasksAsync();
+        }
+
+        private static async Task PrintDelayedRandomTasksAsync()
+        {
+            Random rnd = new();
+            var values = Enumerable.Range(0, 10).Select(_ => rnd.Next(3000)).ToList();
+
+            Console.WriteLine("Initial order: {0}", string.Join(" ", values));
+
+            var tasks = values.Select(DelayAsync);
+
+            var ordered = tasks.OrderByCompletion();
+
+            Console.WriteLine("In order of completion:");
+            await ForEach(ordered, Console.WriteLine);
+        }
+
+        /// <summary>
+        /// Returns a task which delays (asynchronously) by the given number of milliseconds,
+        /// then return that same number back.
+        /// </summary>
+        private static async Task<int> DelayAsync(int delayMillis)
+        {
+            await Task.Delay(delayMillis);
+
+            return delayMillis;
+        }
+
+        /// <summary>
+        /// Executes the given action on each of the tasks in turn, in the order of the sequence.
+        /// The action is passed the result of each task.
+        /// </summary>
+        private static async Task ForEach<T>(IEnumerable<Task<T>> tasks, Action<T> action)
+        {
+            foreach (var task in tasks)
+            {
+                T value = await task;
+
+                action(value);
+            }
+        }
+    }
+}
---
+++ OrderByCompletionExample/TaskExtensions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OrderByCompletionExample
+{
+    public static class TaskExtensions
+    {
+        #region Extension methods
+
+        /// <summary>
+        /// Returns a sequence of tasks which will be observed to complete with the same set of results
+        /// as the given input tasks, but in the order in which the original tasks complete.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="inputTasks"></param>
+        /// <returns></returns>
+        public static IEnumerable<Task<T>> OrderByCompletion<T>(this IEnumerable<Task<T>> inputTasks)
+        {
+            // copy the input so we know it'll be stable, and we don't evaluate it twice
+            var inputTaskList = inputTasks.ToList();
+
+            var completionSourceList = new List<TaskCompletionSource<T>>(inputTaskList.Count);
+            for (int i = 0; i < inputTaskList.Count; i++)
+            {
+                completionSourceList.Add(new TaskCompletionSource<T>());
+            }
+
+            // at any one time, this is "the index of the box we've just filled".
+            // it would be nice to make it newxIndex and start with 0, but Intelocked.Increment
+            // returns the incremented value...
+            int prevIndex = -1;
+
+            // we don't have to create this outside the loop, but it makes it clearer
+            // that the continuation is the same for all tasks.
+            Action<Task<T>> continuation = completedTask =>
+            {
+                int index = Interlocked.Increment(ref prevIndex);
+                var source = completionSourceList[index];
+                PropagateResult(completedTask, source);
+            };
+
+            foreach (var inputTask in inputTaskList)
+            {
+                // TODO: work out whether TaskScheduler.Default is really the right one to use.
+                inputTask.ContinueWith(continuation, CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+            }
+
+            return completionSourceList.Select(source => source.Task);
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static void PropagateResult<T>(Task<T> completedTask, TaskCompletionSource<T> completionSource)
+        {
+            switch (completedTask.Status)
+            {
+                case TaskStatus.Canceled:
+                    completionSource.TrySetCanceled();
+                    break;
+                case TaskStatus.Faulted:
+                    completionSource.TrySetException(completedTask.Exception.InnerExceptions);
+                    break;
+                case TaskStatus.RanToCompletion:
+                    completionSource.TrySetResult(completedTask.Result);
+                    break;
+                default:
+                    throw new ArgumentException("Task was not completed");
+            }
+        }
+
+        #endregion
+    }
+}
