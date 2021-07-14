737b87523a70846b888b0dde50bacb6a97f2c03d
ILepekhov <lepvanek@gmail.com>
2021-07-14 09:51:08 +0500

    Добавлен пример параллельного агрегирования коллекции целых чисел.

---
+++ Concurrency/ParallelAggregation/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParallelAggregation
+{
+    class Program
+    {
+        static void Main()
+        {
+            var intValues = Enumerable.Range(1, 1000);
+
+            Console.WriteLine("ParallelSum result: {0}", ParallelSum(intValues));
+            Console.WriteLine("ParallelSumWithLinqSum result: {0}", ParallelSumWithLinqSum(intValues));
+            Console.WriteLine("ParallelSumWithLinqAggregate result: {0}", ParallelSumWithLinqAggregate(intValues));
+        }
+
+        static int ParallelSum(IEnumerable<int> values)
+        {
+            object mutex = new object();
+
+            int result = 0;
+
+            Parallel.ForEach(source: values,
+                localInit: () => 0,
+                body: (item, state, localValue) => localValue + item,
+                localFinally: localValue =>
+                {
+                    lock (mutex)
+                    {
+                        result += localValue;
+                    }
+                });
+
+            return result;
+        }
+
+        static int ParallelSumWithLinqSum(IEnumerable<int> values)
+        {
+            return values.AsParallel().Sum();
+        }
+
+        static int ParallelSumWithLinqAggregate(IEnumerable<int> values)
+        {
+            return values.AsParallel().Aggregate(
+                seed: 0,
+                func: (sum, item) => sum + item);
+        }
+    }
+}
