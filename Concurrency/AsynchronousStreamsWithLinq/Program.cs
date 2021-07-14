6129a2a6c6477d156dfce745d82c07bbd2610a31
ILepekhov <lepvanek@gmail.com>
2021-07-14 09:39:17 +0500

    Добавлен пример асинхронных потоков с Linq.

---
+++ Concurrency/AsynchronousStreamsWithLinq/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsynchronousStreamsWithLinq
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            IAsyncEnumerable<int> values = SlowRange().WhereAwait(
+                async value =>
+                {
+                    // do some asynchronous work to determine
+                    // if this element should be included
+                    await Task.Delay(10);
+
+                    return value % 2 == 0;
+                });
+
+            await foreach (int result in values)
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+        static async IAsyncEnumerable<int> SlowRange()
+        {
+            for (int i = 0; i != 10; ++i)
+            {
+                await Task.Delay(i * 100);
+
+                yield return i;
+            }
+        }
+    }
+}
