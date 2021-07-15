1f0e329fdf4de77e0cb788801302e6aed41d7015
ILepekhov <lepvanek@gmail.com>
2021-07-15 17:41:44 +0500

    Добавлен пример отмены TaskCompletionSource.

---
+++ Concurrency/TaskCompletionSourceCancellation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskCompletionSourceCancellation
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Task cancelTask = Task.Run(() =>
+            {
+                while (Console.ReadKey().Key != ConsoleKey.Enter)
+                {
+                    Console.WriteLine("Press the ENTER key to cancel...");
+                }
+
+                Console.WriteLine("\nENTER key pressed: cancelling downloads.\n");
+            });
+
+            var cts = new CancellationTokenSource(10000);
+
+            var tcs = new TaskCompletionSource<object>();
+
+            Task.Delay(10000).ContinueWith(_ => tcs.TrySetCanceled());
+
+            var finishedTask = await Task.WhenAny(cancelTask, tcs.Task);
+
+            if (finishedTask == cancelTask)
+            {
+                Console.WriteLine("Вышли по запросу пользователя");
+            }
+            else if (finishedTask == tcs.Task)
+            {
+                Console.WriteLine("Вышли по таймеру cts");
+            }
+            else
+            {
+                Console.WriteLine("А вот это странно!");
+            }
+        }
+    }
+}
