589a1950469566af60498bd492c1f2bc7706e7a4
ILepekhov <lepvanek@gmail.com>
2021-07-19 17:54:08 +0500

    Добавил пример рефлексии record'ов.

---
+++ RecordsTesting/Point.cs
@@ -0,0 +1,4 @@
+﻿namespace RecordsTesting
+{
+    public record Point(int X, int Y);
+}
---
+++ RecordsTesting/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RecordsTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TryModifyRecord();
+        }
+
+        static void TryModifyRecord()
+        {
+            Console.WriteLine("====TryModifyRecord====");
+
+            Point point = new(1, 2);
+
+            Console.WriteLine("Initial 'point' state: {0}", point);
+
+            var pointType = typeof(Point);
+            var xProp = pointType.GetProperty(nameof(Point.X));
+            if (xProp is not null)
+            {
+                xProp.SetValue(point, 3);
+            }
+            else
+            {
+                Console.WriteLine("Can't get access to the X property");
+            }
+
+            Console.WriteLine("Final 'point' state: {0}", point);
+
+            Console.WriteLine();
+        }
+    }
+}
