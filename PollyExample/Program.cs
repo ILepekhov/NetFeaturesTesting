57aedb60c6041cadd2186aa872af4c455dac6641
ILepekhov <lepvanek@gmail.com>
2021-07-19 09:59:05 +0500

    Добавлен пример использования Polly с повторами действия при исключениях.

---
+++ PollyExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using Polly;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PollyExample
+{
+    class Program
+    {
+        static int _counter;
+
+        static async Task Main()
+        {
+            _counter = 0;
+
+            var policy = Policy
+                .Handle<TimeoutException>()
+                .RetryAsync(4, (exception, retryCount, context) =>
+                {
+                    Console.WriteLine($"{DateTime.Now.ToLongTimeString()} try: {retryCount}, Exception: {exception.Message}");
+                });
+
+            var result = await policy.ExecuteAsync(async () => await AsyncOperation());
+
+            Console.WriteLine($"{DateTime.Now.ToLongTimeString()} {result}");
+        }
+
+        static async Task<string> AsyncOperation()
+        {
+            if (_counter < 3)
+            {
+                var index = Interlocked.Increment(ref _counter);
+
+                await Task.Delay(index * 5 * 1000);
+
+                throw new TimeoutException("Blech.");
+            }
+
+            return await Task.FromResult("Yep!");
+        }
+    }
+}
