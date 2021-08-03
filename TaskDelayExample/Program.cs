d9e58946031132bad88acb200a84e8002418553e
ILepekhov <lepvanek@gmail.com>
2021-08-03 13:56:31 +0500

    Добавил пример поведения Task.Delay с применением токена отмены

---
+++ TaskDelayExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskDelayExample
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.Write("Enter delay in seconds: ");
+            int delay = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter cancellation timeout in seconds: ");
+            int tiemout = Convert.ToInt32(Console.ReadLine());
+
+            CancellationTokenSource cts = new CancellationTokenSource(TimeSpan.FromSeconds(tiemout));
+
+            try
+            {
+                Console.WriteLine("Start waiting");
+
+                await Task.Delay(TimeSpan.FromSeconds(delay), cts.Token);
+
+                Console.WriteLine("It worked out fine");
+            }
+            catch (TaskCanceledException)
+            {
+                Console.WriteLine("Delay task was cancelled");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unexpected exception");
+            }
+        }
+    }
+}
