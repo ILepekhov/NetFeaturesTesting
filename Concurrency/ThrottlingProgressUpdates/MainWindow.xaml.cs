53e779c1444cd390e5e24da9d693091df13b8007
ILepekhov <lepvanek@gmail.com>
2021-08-05 10:17:19 +0500

    Добавлен пример дросселирования событий, отправляемых в UI поток с помощью System.Reactive.

---
+++ Concurrency/ThrottlingProgressUpdates/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ThrottlingProgressUpdates
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (EnableThrottlingCheckBox.IsChecked == true)
+            {
+                ProgressValueLabel.Text = "Starting...";
+
+                var (observable, progress) = ObservableProgress.CreateForUi<int>();
+
+                string result = string.Empty;
+
+                using (observable.Subscribe(value => ProgressValueLabel.Text = value.ToString()))
+                {
+                    result = await Task.Run(() => Solve(progress));
+                }
+
+                ProgressValueLabel.Text = $"Done! Result: {result}";
+            }
+            else
+            {
+                ProgressValueLabel.Text = "Starting...";
+
+                var progress = new Progress<int>(value => ProgressValueLabel.Text = value.ToString());
+
+                var result = await Task.Run(() => Solve(progress));
+
+                ProgressValueLabel.Text = $"Done! Result: {result}";
+            }
+        }
+
+        private string Solve(IProgress<int> progress)
+        {
+            var endTime = DateTime.UtcNow.AddSeconds(3);
+
+            int value = 0;
+
+            while (DateTime.UtcNow < endTime)
+            {
+                value++;
+
+                progress?.Report(value);
+            }
+
+            return value.ToString();
+        }
+    }
+}
---
+++ Concurrency/ThrottlingProgressUpdates/ObservableProgress.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace ThrottlingProgressUpdates
+{
+    public static class ObservableProgress
+    {
+        private sealed class EventProgress<T> : IProgress<T>
+        {
+            void IProgress<T>.Report(T value) => OnReport?.Invoke(value);
+            public event Action<T> OnReport;
+        }
+
+        public static (IObservable<T>, IProgress<T>) Create<T>()
+        {
+            var progress = new EventProgress<T>();
+            var observable = Observable.FromEvent<T>(
+                handler => progress.OnReport += handler,
+                handler => progress.OnReport -= handler);
+
+            return (observable, progress);
+        }
+
+        // Note: this must be called from the UI thread
+        public static (IObservable<T>, IProgress<T>) CreateForUi<T>(TimeSpan? sampleInterval = null)
+        {
+            var (observable, progress) = Create<T>();
+
+            observable = observable
+                .Sample(sampleInterval ?? TimeSpan.FromMilliseconds(100))
+                .ObserveOn(SynchronizationContext.Current);
+
+            return (observable, progress);
+        }
+    }
+}
