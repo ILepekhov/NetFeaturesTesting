9374ac878e1d658298d7edb423ef6548d750e711
ILepekhov <lepvanek@gmail.com>
2021-08-13 13:13:20 +0500

    Добавлен пример использования блока try..finally и в цикле с прерыванием цилка.

---
+++ TryFinallyLoopBrakeTesting/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TryFinallyLoopBrakeTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var intArray = new[] { 1, 2, 3 };
+
+            foreach (var item in intArray)
+            {
+                Console.WriteLine($"Next item is '{item}'");
+
+                try
+                {
+                    Console.WriteLine($"Item '{item}'. Enter into a try..finally body");
+
+                    if (item > 2)
+                    {
+                        Console.WriteLine($"Item '{item}'. Breaking a loop");
+                        break;
+                    }
+                }
+                finally
+                {
+                    Console.WriteLine($"Item '{item}'. Finally branch executed");
+                }
+            }
+
+            Console.WriteLine("Loop finished");
+        }
+    }
+}
