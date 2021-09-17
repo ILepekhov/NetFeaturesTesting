60c0924c2457f252f9ec288f798ee1c0e67f4770
ILepekhov <lepvanek@gmail.com>
2021-09-17 10:00:01 +0500

    Добавлен пример создания Observable из Task.

---
+++ System.Reactive/SubscribeAsyncExcerise/Program.cs
@@ -0,0 +1,85 @@
+﻿using ExtensionsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Threading;
+
+namespace SubscribeAsyncExcerise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var disposable = Search2("abc")
+                .Timestamp()
+                .SubscribeConsole("search");
+
+            while (true)
+            {
+                Console.WriteLine("Press 'C' to dispose observer or 'Enter' to close app");
+
+                var key = Console.ReadKey().Key;
+
+                if (key == ConsoleKey.C)
+                {
+                    disposable.Dispose();
+                    break;
+                }
+                else if (key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+            }
+
+            Thread.Sleep(2000);
+        }
+
+        private static IObservable<string> Search(string query)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                throw new ArgumentException($"'{nameof(query)}' cannot be null or whitespace.", nameof(query));
+            }
+
+            return Observable.Create<string>(async (o, ct) =>
+            {
+                var searchEngineA = new SearchEngine("Engine A");
+                var searchEngineB = new SearchEngine("Engine B");
+
+                var resultsA = await searchEngineA.SearchAsync(query, ct);
+                foreach (var result in resultsA)
+                {
+                    ct.ThrowIfCancellationRequested();
+
+                    o.OnNext(result);
+                }
+
+                var resultsB = await searchEngineB.SearchAsync(query, ct);
+                foreach (var result in resultsB)
+                {
+                    ct.ThrowIfCancellationRequested();
+
+                    o.OnNext(result);
+                }
+
+                ct.ThrowIfCancellationRequested();
+
+                o.OnCompleted();
+            });
+        }
+
+        private static IObservable<string> Search2(string query)
+        {
+            var searchEngineA = new SearchEngine("Engine A");
+            var searchEngineB = new SearchEngine("Engine B");
+
+            IObservable<IEnumerable<string>> resultsA = searchEngineA.SearchAsync(query, default).ToObservable();
+            IObservable<IEnumerable<string>> resultsB = searchEngineB.SearchAsync(query, default).ToObservable();
+
+            return resultsA
+                .Concat(resultsB)
+                .SelectMany(results => results);
+        }
+    }
+}
---
+++ System.Reactive/SubscribeAsyncExcerise/SearchEngine.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SubscribeAsyncExcerise
+{
+    internal class SearchEngine
+    {
+        #region Fields
+
+        private readonly string _searchEngineName;
+
+        #endregion
+
+        #region Constructor
+
+        public SearchEngine(string searchEngineName)
+        {
+            if (string.IsNullOrWhiteSpace(searchEngineName))
+            {
+                throw new ArgumentException($"'{nameof(searchEngineName)}' cannot be null or whitespace.", nameof(searchEngineName));
+            }
+
+            _searchEngineName = searchEngineName;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public async Task<List<string>> SearchAsync(string query, CancellationToken cancellationToken)
+        {
+            List<string> results = new();
+
+            await Task.Delay(2000);
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            results.Add($"{_searchEngineName} ({query}): {Guid.NewGuid()}");
+
+            await Task.Delay(2000);
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            results.Add($"{_searchEngineName} ({query}): {Guid.NewGuid()}");
+
+            return results;
+        }
+
+        #endregion
+    }
+}
