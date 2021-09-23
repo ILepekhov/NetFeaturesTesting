3c19c89030ce266997cf12c13b7b1b130f29c336
ILepekhov <lepvanek@gmail.com>
2021-09-23 10:15:38 +0500

    Добавлен пример отложенной подписки и завершения подписки по сигналу.

---
+++ System.Reactive/ObservationStartStopConditions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using ExtensionsLibrary;
+
+namespace ObservationStartStopConditions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DelayedSubscriptionExample(TimeSpan.FromSeconds(2))
+                .SubscribeConsole();
+
+            Console.ReadLine();
+
+            LimitedBySignalSubscriptionExample().SubscribeConsole();
+
+            Console.ReadLine();
+        }
+
+        private static IObservable<Timestamped<int>> DelayedSubscriptionExample(TimeSpan delay)
+        {
+            Console.WriteLine("Startup time is {0}", DateTime.Now);
+
+            return Observable.Range(1, 5)
+                .Timestamp()
+                .DelaySubscription(delay);
+        }
+
+        private static IObservable<DateTimeOffset> LimitedBySignalSubscriptionExample()
+        {
+            return Observable.Timer(DateTimeOffset.Now, TimeSpan.FromSeconds(1))
+                .Select(t => DateTimeOffset.Now)
+                .TakeUntil(Observable.Timer(TimeSpan.FromSeconds(5)));
+        }
+    }
+}
