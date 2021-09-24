e20493acef6194466bbadeb8e07fbe0634b61025
ILepekhov <lepvanek@gmail.com>
2021-09-24 10:05:19 +0500

    Добавлен пример фильтрации с помощью SkipWhile и TakeWhile.

---
+++ System.Reactive/SkipWhileTakeWhileExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using ExtensionsLibrary;
+using System;
+using System.Reactive.Linq;
+
+namespace SkipWhileTakeWhileExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Observable.Range(1, 10)
+                .SkipWhile(x => x < 2)
+                .TakeWhile(x => x < 7)
+                .SubscribeConsole();
+
+            Console.ReadLine();
+        }
+    }
+}
