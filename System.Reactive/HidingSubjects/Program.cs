d63f2ad4d1054fe0b775f1ab2a1f60e4080d3ce1
ILepekhov <lepvanek@gmail.com>
2021-09-29 10:00:29 +0500

    Added the example of hiding Subjects with .AsObservable extension method.

---
+++ System.Reactive/HidingSubjects/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace HidingSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject<int> sbj = new Subject<int>();
+
+            var proxy = sbj.AsObservable(); // now i can expose proxy as IObservable<int> property in a class for example
+
+            var castToSubject = proxy as Subject<int>;
+            var castToObserver = proxy as IObserver<int>;
+
+            Console.WriteLine("proxy as Subject<int> is '{0}'", castToSubject is null ? "null" : "not null");
+            Console.WriteLine("proxy as IObserver<int> is '{0}'", castToObserver is null ? "null" : "not null");
+        }
+    }
+}
