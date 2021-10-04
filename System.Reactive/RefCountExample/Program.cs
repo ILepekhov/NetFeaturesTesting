e90989e97cd5afe882a65b71496fd8cecb3f4cbd
ILepekhov <lepvanek@gmail.com>
2021-10-04 10:04:29 +0500

    Added an example of using RefCount with Publish to heating a cold observable with authimatic disconnection.

---
+++ System.Reactive/RefCountExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using ExtensionsLibrary;
+using System;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace RefCountExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // heating cool observable with authomatic disconnection
+            // when all observers will be desposed
+            var publishedObservable = Observable.Interval(TimeSpan.FromSeconds(1))
+                .Do(x => Console.WriteLine("Generating {0}", x))
+                .Publish()
+                .RefCount();
+
+            var subscription1 = publishedObservable.SubscribeConsole("First");
+            var subscription2 = publishedObservable.SubscribeConsole("Second");
+
+            Thread.Sleep(3000);
+            subscription1.Dispose();
+            Thread.Sleep(3000);
+            subscription2.Dispose();
+
+            Console.ReadLine();
+        }
+    }
+}
