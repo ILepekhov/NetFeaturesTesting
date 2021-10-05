a9dfafdf523aa553aee3ec1763304d1fef832e58
ILepekhov <lepvanek@gmail.com>
2021-10-05 09:49:53 +0500

    Added an example of "Cooling" of the hot observable using "Replay" operator.

---
+++ System.Reactive/CoolingHotObservableExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using ExtensionsLibrary;
+using System;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace CoolingHotObservableExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var publishedObservable = Observable.Interval(TimeSpan.FromSeconds(1)) // <- the hot Observable imitation
+                .Take(10)
+                .Timestamp()
+                .Replay(window: TimeSpan.FromSeconds(7)); // <- stores emissions for the last 7 seconds and replays them to the new observers
+
+            publishedObservable.Connect();
+
+            var subscription1 = publishedObservable.SubscribeConsole("First");
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            var subscription2 = publishedObservable.SubscribeConsole("Second");
+
+            Console.ReadLine();
+        }
+    }
+}
