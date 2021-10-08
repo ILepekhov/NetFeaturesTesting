49eaefc73d2fd4d5cd3cf80a65b13e51aea39646
ILepekhov <lepvanek@gmail.com>
2021-10-08 09:39:55 +0500

    Added a WPF app example of using couple of DisctinctUntilChanged and Throttle operators.

---
+++ System.Reactive/DistinctUntilChangedExample/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DistinctUntilChangedExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (SynchronizationContext.Current is not null)
+            {
+                Observable.FromEventPattern(SearchBox, nameof(TextBox.TextChanged))
+                    .Select(_ => SearchBox.Text)
+                    .Throttle(TimeSpan.FromMilliseconds(400))
+                    .DistinctUntilChanged()
+                    .ObserveOn(SynchronizationContext.Current)
+                    .Subscribe(s => ResultsBox.Items.Add(s));
+            }
+        }
+    }
+}
