6f63c06579bc4a93c4b97bfc1b7516d96ef10f49
ILepekhov <lepvanek@gmail.com>
2021-10-11 10:06:04 +0500

    Added an example of using Aggregate operator with ResultSelector (overload)

---
+++ System.Reactive/AggregateWithResultSelector/Program.cs
@@ -0,0 +1,34 @@
+﻿using ExtensionsLibrary;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+Subject<int> numbers = new Subject<int>();
+
+numbers.Aggregate(
+    new SortedSet<int>(),
+    (largest, item) =>
+    {
+        largest.Add(item);
+
+        if (largest.Count() > 2)
+        {
+            largest.Remove(largest.First());
+        }
+
+        return largest;
+    },
+    largest => largest.FirstOrDefault())
+    .SubscribeConsole();
+
+var numbersCollection = new[] { 3, 1, 4, 2, 5 };
+
+foreach (var number in numbersCollection)
+{
+    Console.WriteLine($"numbers.OnNext({number});");
+    numbers.OnNext(number);
+}
+
+Console.WriteLine("numbers.OnCompleted()");
+numbers.OnCompleted();
+
+Console.ReadLine();
