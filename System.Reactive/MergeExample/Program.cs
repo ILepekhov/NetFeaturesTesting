028ddd8ba02dde9ad97924f3a9e8c7fc4ef7c518
ILepekhov <lepvanek@gmail.com>
2021-10-12 09:52:34 +0500

    Added an example of using "Merge" operator

---
+++ System.Reactive/MergeExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExtensionsLibrary;
+using System;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+
+Task<string[]> facebookMessages = Task.Delay(TimeSpan.FromSeconds(1)).ContinueWith(_ => new[] { "Facebook-1", "Facebook-2" });
+Task<string[]> twitterMessages = Task.FromResult(new[] { "Twitter-1", "Twitter-2" });
+
+Observable.Merge(facebookMessages.ToObservable(), twitterMessages.ToObservable())
+    .SelectMany(messages => messages)
+    .SubscribeConsole("Merge Messages");
+
+Console.ReadLine();
