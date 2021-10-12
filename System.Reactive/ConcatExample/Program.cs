19f39a3a196f98ff29badfbfb0aac2d1a202bf00
ILepekhov <lepvanek@gmail.com>
2021-10-12 09:47:26 +0500

    Added an example of using "Concat" operator.

---
+++ System.Reactive/ConcatExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using ExtensionsLibrary;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+
+Task<string[]> facebookMessages = Task.Delay(TimeSpan.FromSeconds(1)).ContinueWith(_ => new[] { "Facebook-1", "Facebook-2" });
+Task<string[]> twitterMessages = Task.FromResult(new[] { "Twitter-1", "Twitter-2" });
+
+Observable.Concat(facebookMessages.ToObservable(), twitterMessages.ToObservable())
+    .SelectMany(messages => messages)
+    .SubscribeConsole("Concat Messages");
+
+Console.ReadLine();
