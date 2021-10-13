7e44472c3da202c365412c8a5b51ffabd4ff6bf5
ILepekhov <lepvanek@gmail.com>
2021-10-13 09:29:30 +0500

    Added an example of dynamic concatenating and merging of observables

---
+++ System.Reactive/DynamicConcatenatingAndMerging/Program.cs
@@ -0,0 +1,39 @@
+﻿using ExtensionsLibrary;
+using System.Reactive.Linq;
+
+// MergeAllObservablesSimultaneously();
+MergeObservablesWithConcurrencyLimitations();
+
+Console.ReadLine();
+
+void MergeAllObservablesSimultaneously()
+{
+    IObservable<string> texts = new[] { "Hello", "World" }.ToObservable();
+
+    texts
+        .Select(txt => Observable.Return(txt + "-Result"))
+        .Merge() // IObservable<IObservale<string>> -> IObservable<string>
+        .SubscribeConsole();
+}
+
+void MergeObservablesWithConcurrencyLimitations()
+{
+    IObservable<string> first = Observable.Interval(TimeSpan.FromSeconds(1))
+        .Select(i => "First " + i)
+        .Do(x => Console.WriteLine("DO " + x))
+        .Take(2);
+
+    IObservable<string> second = Observable.Interval(TimeSpan.FromSeconds(1))
+        .Select(i => "Second " + i)
+        .Do(x => Console.WriteLine("DO " + x))
+        .Take(2);
+
+    IObservable<string> third = Observable.Interval(TimeSpan.FromSeconds(1))
+        .Select(i => "Third " + i)
+        .Do(x => Console.WriteLine("DO " + x))
+        .Take(2);
+
+    new[] { first, second, third }.ToObservable()
+        .Merge(2) // setting limit for the simultaneous emissions
+        .SubscribeConsole("Merge with 2 concurrent subscriptions");
+}
