5a9b02cd9a6f224e5aa6772ee481e9e764d9540d
ILepekhov <lepvanek@gmail.com>
2021-10-18 09:50:47 +0500

    Added an example of using the "Window" operator.

---
+++ System.Reactive/WindowingExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using ExtensionsLibrary;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+Subject<decimal> donationsSubject = new();
+IObservable<decimal> donations = donationsSubject.AsObservable();
+
+Console.WriteLine("Shift 1: 50$, 55$, 60$");
+Console.WriteLine("Shift 2: 49$, 48$, 45$");
+
+var windows = donations.Window(TimeSpan.FromSeconds(1));
+
+var donationsSums =
+    from window in windows.Do(_ => Console.WriteLine("New window"))
+    from sum in window.Scan((prevSum, donation) => prevSum + donation)
+    select sum;
+
+donationsSums.SubscribeConsole("donations in shift");
+
+donationsSubject.OnNext(50);
+donationsSubject.OnNext(55);
+donationsSubject.OnNext(60);
+
+Thread.Sleep(TimeSpan.FromMilliseconds(1100));
+
+donationsSubject.OnNext(49);
+donationsSubject.OnNext(48);
+donationsSubject.OnNext(45);
+
+donationsSubject.OnCompleted();
+
+Console.ReadLine();
