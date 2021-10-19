aadf44209667282fbf5bade8fd7d2507af4bde47
ILepekhov <lepvanek@gmail.com>
2021-10-19 10:22:16 +0500

    Added an example of the manual defining the schedulers.

---
+++ System.Reactive/ConcurrencyAndSynchonization/DefiningTheScheduler/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+
+//SimpleExample();
+MoreAdvancedExample();
+
+Console.ReadLine();
+
+void SimpleExample()
+{
+    IScheduler scheduler = NewThreadScheduler.Default;
+
+    IDisposable scheduling = scheduler.Schedule(
+        Unit.Default,
+        TimeSpan.FromSeconds(2),
+        (scdlr, _) =>
+        {
+            Console.WriteLine("Hello World, Now {0}", scdlr.Now);
+
+            return Disposable.Empty;
+        });
+}
+
+void MoreAdvancedExample()
+{
+    IScheduler scheduler = NewThreadScheduler.Default;
+
+    Func<IScheduler, int, IDisposable> action = null;
+
+    action = (scdlr, callNumber) =>
+    {
+        Console.WriteLine($"Hello {callNumber}, Now: {scdlr.Now}, Thread: {Thread.CurrentThread.ManagedThreadId}");
+
+        return scdlr.Schedule(callNumber + 1, TimeSpan.FromSeconds(2), action);
+    };
+
+    IDisposable scheduling = scheduler.Schedule(0, TimeSpan.FromSeconds(2), action);
+
+    Console.WriteLine("Press Enter to stop");
+
+    Console.ReadLine();
+
+    scheduling.Dispose();
+}
