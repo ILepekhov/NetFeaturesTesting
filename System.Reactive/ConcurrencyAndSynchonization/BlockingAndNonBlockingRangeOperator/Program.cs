c89ae5d2a2af7ce2f1934ff01e4a6be4cef64cac
ILepekhov <lepvanek@gmail.com>
2021-10-20 09:41:30 +0500

    Added an example of locking and unlocking current thread with the Range operator.

---
+++ System.Reactive/ConcurrencyAndSynchonization/BlockingAndNonBlockingRangeOperator/Program.cs
@@ -0,0 +1,33 @@
+﻿using ExtensionsLibrary;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+//RangeWithBlocking();
+RangeWithNoBlocking();
+
+Console.WriteLine("Press Enter to stop");
+Console.ReadLine();
+
+void RangeWithBlocking()
+{
+    Console.WriteLine($"RangeWithBlocking, threadId {Thread.CurrentThread.ManagedThreadId}");
+
+    var subscription = Observable.Range(1, 5)
+        .Repeat()
+        .SubscribeConsole($"RangeWithNoBlocking, threadId {Thread.CurrentThread.ManagedThreadId}");
+
+    // won't come here
+
+    subscription.Dispose();
+}
+
+void RangeWithNoBlocking()
+{
+    Console.WriteLine($"RangeWithNoBlocking, threadId {Thread.CurrentThread.ManagedThreadId}");
+
+    var subscription = Observable.Range(1, 5, NewThreadScheduler.Default)
+        .Repeat()
+        .SubscribeConsole($"RangeWithNoBlocking, threadId {Thread.CurrentThread.ManagedThreadId}");
+
+    subscription.Dispose();
+}
