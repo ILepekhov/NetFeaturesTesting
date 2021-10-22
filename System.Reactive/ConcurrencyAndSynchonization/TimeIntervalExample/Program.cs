afdb9a4d1af215a3851bc49b329d127cf81279f2
ILepekhov <lepvanek@gmail.com>
2021-10-22 09:33:34 +0500

    Added an example of measuring the time intervals between emissions (with the TimeInterval operator)

---
+++ System.Reactive/ConcurrencyAndSynchonization/TimeIntervalExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExtensionsLibrary;
+using System.Reactive.Linq;
+
+var deviceHeartbeat = Observable
+    .Timer(TimeSpan.FromSeconds(1))
+    .Concat(Observable.Timer(TimeSpan.FromSeconds(2)))
+    .Concat(Observable.Timer(TimeSpan.FromSeconds(4)));
+
+deviceHeartbeat
+    .TimeInterval() // <-- measures the time intervals between two emissions
+    .SubscribeConsole("Time from last heartbeat");
+
+Console.ReadLine();
