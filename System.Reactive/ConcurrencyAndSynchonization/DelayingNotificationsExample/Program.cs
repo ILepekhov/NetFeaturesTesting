e22f31d2a475541d80834898f606e49d5d4b20d3
ILepekhov <lepvanek@gmail.com>
2021-10-22 10:31:36 +0500

    Added the examples of delaying notifications.

---
+++ System.Reactive/ConcurrencyAndSynchonization/DelayingNotificationsExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using ExtensionsLibrary;
+using System.Reactive.Linq;
+
+var observable = Observable
+    .Timer(TimeSpan.FromSeconds(1))
+    .Concat(Observable.Timer(TimeSpan.FromSeconds(1)))
+    .Concat(Observable.Timer(TimeSpan.FromSeconds(4)))
+    .Concat(Observable.Timer(TimeSpan.FromSeconds(4)));
+
+observable
+    .Timestamp()
+    .Delay(TimeSpan.FromSeconds(2))
+    .Timestamp()
+    .Take(5)
+    .SubscribeConsole("Delay");
+
+Console.ReadLine();
---
+++ System.Reactive/ConcurrencyAndSynchonization/DelayingNotificationsIndependentlyExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using ExtensionsLibrary;
+using System.Reactive.Linq;
+
+var observable = new[] { 4, 1, 3, 2 }.ToObservable();
+
+observable
+    .Timestamp()
+    .Delay(x => Observable.Timer(TimeSpan.FromSeconds(x.Value)))
+    .Timestamp()
+    .SubscribeConsole("Delay");
+
+Console.ReadLine();
