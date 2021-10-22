3d7f1013bac075f71557a7dc521932b30c298b68
ILepekhov <lepvanek@gmail.com>
2021-10-22 09:55:29 +0500

    Added example of the closing subscription by the timeout (with the Timeout operator).

---
+++ System.Reactive/ConcurrencyAndSynchonization/TimeoutExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExtensionsLibrary;
+using System.Reactive.Linq;
+
+var observable = Observable
+    .Timer(TimeSpan.FromSeconds(1))
+    .Concat(Observable.Timer(TimeSpan.FromSeconds(4)))
+    .Concat(Observable.Timer(TimeSpan.FromSeconds(4)));
+
+observable
+    .Timeout(TimeSpan.FromSeconds(3))
+    .SubscribeConsole("Timeout");
+
+Console.ReadLine();
