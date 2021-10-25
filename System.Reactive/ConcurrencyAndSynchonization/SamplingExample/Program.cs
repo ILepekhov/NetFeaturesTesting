97cec4131dbc47d59bb921e395edff4d7aa80133
ILepekhov <lepvanek@gmail.com>
2021-10-25 10:00:50 +0500

    Added notifications sampling example.

---
+++ System.Reactive/ConcurrencyAndSynchonization/SamplingExample/Program.cs
@@ -0,0 +1,10 @@
+﻿using ExtensionsLibrary;
+using System.Reactive.Linq;
+
+Observable
+    .Interval(TimeSpan.FromSeconds(1))
+    .Sample(TimeSpan.FromSeconds(3.5))
+    .Take(3)
+    .SubscribeConsole("Sample");
+
+Console.ReadLine();
