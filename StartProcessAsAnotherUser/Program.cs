a8f9bd8d6d1aeddf9fe3f23bf6072b39573841f2
ILepekhov <lepvanek@gmail.com>
2021-10-25 13:12:48 +0500

    Added an example of starting process with specified user credentials

---
+++ StartProcessAsAnotherUser/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Security;
+
+Console.WriteLine("This utility will start the 'cmd.exe' with the specified credentials");
+
+Console.Write("Enter username: ");
+string? username = Console.ReadLine();
+
+Console.Write("Enter password: ");
+string? password = Console.ReadLine();
+SecureString passwordSecureString = new();
+
+foreach (var letter in password)
+{
+    passwordSecureString.AppendChar(letter);
+}
+
+Process process = new();
+
+process.StartInfo.FileName = "cmd.exe";
+process.StartInfo.UseShellExecute = false;
+process.StartInfo.UserName = username;
+process.StartInfo.Password = passwordSecureString;
+
+process.Start();
+
+Console.WriteLine("Press 'Enter' to exit...");
+Console.ReadLine();
