9afa7499e36eea124620220bb90787d0612271ee
ILepekhov <lepvanek@gmail.com>
2021-10-25 10:15:30 +0500

    Added an example of changing the subscription/unsubscription execution context.

---
+++ System.Reactive/ConcurrencyAndSynchonization/ChangingTheSubscriptionUnsubscritionExecutionContext/Program.cs
@@ -0,0 +1,30 @@
+﻿using ExtensionsLibrary;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+var eventLoopScheduler = new EventLoopScheduler();
+
+var subscription = Observable
+    .Interval(TimeSpan.FromSeconds(1))
+    .Do(x => Console.WriteLine("Inside Do ({0})", x))
+    .SubscribeOn(eventLoopScheduler)
+    .SubscribeConsole();
+
+Thread.Sleep(TimeSpan.FromSeconds(1));
+
+eventLoopScheduler.Schedule(1, (s, state) =>
+{
+    Console.WriteLine("Before sleep");
+    Thread.Sleep(TimeSpan.FromSeconds(3));
+    Console.WriteLine("After sleep");
+
+    return Disposable.Empty;
+});
+
+
+
+subscription.Dispose();
+
+Console.WriteLine("Subscription disposed");
+Console.ReadLine();
