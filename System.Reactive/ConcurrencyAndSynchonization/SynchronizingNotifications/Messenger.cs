64e03b0fad572df1f2704a16024f638b2ad29892
ILepekhov <lepvanek@gmail.com>
2021-10-26 10:22:54 +0500

    Added an example of the synchronizing emissions to the observer.

---
+++ System.Reactive/ConcurrencyAndSynchonization/SynchronizingNotifications/Messenger.cs
@@ -0,0 +1,20 @@
+﻿internal class Messenger
+{
+    #region Events
+
+    public event EventHandler<string>? MessageReceived;
+
+    #endregion
+
+    #region Methods
+
+    public void RaiseMessageReceived()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Task.Factory.StartNew(obj => MessageReceived?.Invoke(this, obj.ToString()), "Msg" + i);
+        }
+    }
+
+    #endregion
+}
---
+++ System.Reactive/ConcurrencyAndSynchonization/SynchronizingNotifications/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reactive.Linq;
+
+var messenger = new Messenger();
+var messages = Observable
+    .FromEventPattern<string>(
+        h => messenger.MessageReceived += h,
+        h => messenger.MessageReceived -= h);
+
+messages
+    .Select(evt => evt.EventArgs)
+    .Synchronize()
+    .Subscribe(msg =>
+    {
+        Console.WriteLine($"Message {msg} arrived");
+        Thread.Sleep(1000);
+        Console.WriteLine($"Message {msg} processed");
+    });
+
+messenger.RaiseMessageReceived();
+
+Console.ReadLine();
+
+
