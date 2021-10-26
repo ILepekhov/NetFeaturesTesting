6a703fe94786bf09081d2efe1c411953bb93fc75
ILepekhov <lepvanek@gmail.com>
2021-10-26 09:57:35 +0500

    Added an example of the subscription and the observation in the different threads.

---
+++ System.Reactive/ConcurrencyAndSynchonization/UsingSubscribeOnAndObserveOnTogether/IObservableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Reactive.Linq;
+
+internal static class IObservableExtensions
+{
+    public static IObservable<T> LogWithThread<T>(this IObservable<T> observable, string title = "")
+    {
+        return Observable.Defer(() =>
+        {
+            Console.WriteLine($"{title} Subscription happened on Thread: {Environment.CurrentManagedThreadId}");
+
+            return observable.Do(
+                x => Console.WriteLine($"{title} - OnNext({x}) Thread: {Environment.CurrentManagedThreadId}"),
+                ex =>
+                {
+                    Console.WriteLine($"{title} - OnError() Thread: {Environment.CurrentManagedThreadId}");
+                    Console.WriteLine($"\t {ex}");
+                },
+                () => Console.WriteLine($"{title} - OnCompleted() Thread: {Environment.CurrentManagedThreadId}"));
+        });
+    }
+}
---
+++ System.Reactive/ConcurrencyAndSynchonization/UsingSubscribeOnAndObserveOnTogether/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExtensionsLibrary;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+new[] { 0, 1, 2, 3, 4, 5 }.ToObservable()
+    .Take(3).LogWithThread("A")
+    .Where(x => x % 2 == 0).LogWithThread("B")
+    .SubscribeOn(NewThreadScheduler.Default).LogWithThread("C")
+    .Select(x => x * x).LogWithThread("D")
+    .ObserveOn(TaskPoolScheduler.Default).LogWithThread("E")
+    .SubscribeConsole("squares by time");
+
+Console.ReadLine();
