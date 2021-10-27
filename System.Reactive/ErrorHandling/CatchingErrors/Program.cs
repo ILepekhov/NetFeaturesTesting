c1b0be38089be87f583862801f15ea041d2e3819
ILepekhov <lepvanek@gmail.com>
2021-10-27 09:56:11 +0500

    Added an example of catching the certain exception (OnError) from Observable with resubscribing to the fallback Observable.

---
+++ System.Reactive/ErrorHandling/CatchingErrors/Program.cs
@@ -0,0 +1,16 @@
+﻿using ExtensionsLibrary;
+using System.Reactive.Linq;
+
+IObservable<string> weatherSimulationResults = Observable
+    .Throw<string>(new OutOfMemoryException());
+
+weatherSimulationResults
+    .Catch((OutOfMemoryException ex) =>
+    {
+        Console.WriteLine("Handling OOM exception");
+
+        return Observable.Empty<string>();
+    })
+    .SubscribeConsole("Catch (source throws)");
+
+Console.ReadLine();
