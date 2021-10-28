33302fa9b423f0f0ac873aa9e26645323675654a
ILepekhov <lepvanek@gmail.com>
2021-10-28 09:40:25 +0500

    Added an example of switching to a fallback observable in case of error or completion.

---
+++ System.Reactive/ErrorHandling/OnErrorResumeNext/Program.cs
@@ -0,0 +1,20 @@
+﻿using ExtensionsLibrary;
+using System.Reactive.Linq;
+
+IObservable<string> weatherReportA =
+    Observable.Throw<string>(new OutOfMemoryException());
+
+IObservable<string> weatherReportB =
+    Observable.Return("The weather B is OK");
+
+weatherReportA
+    .OnErrorResumeNext(weatherReportB)
+    .SubscribeConsole("OnErrorResumeNext (source throws)");
+
+weatherReportB
+    .OnErrorResumeNext(weatherReportB)
+    .SubscribeConsole("OnErrorResumeNext (source completed)");
+
+// The difference between the 'Catch' and the 'OnErrorResumeNext' is that the 'OnErrorResumeNext' operator
+// will switch to fallback observable both on error and on completed states, so 'Catch' - only on error.
+// 'OnErrorResumeNext' is the mix of the 'Catch' and the 'Concat' operators.
