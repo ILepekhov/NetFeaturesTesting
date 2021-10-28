aca55e84c21fa20320e2444db2f9d35dd11e7bfc
ILepekhov <lepvanek@gmail.com>
2021-10-28 09:46:02 +0500

    Added an example of the retrying to subscribe to the source observable in case of an error.

---
+++ System.Reactive/ErrorHandling/RetryingToSubscribeInCaseOfAnError/Program.cs
@@ -0,0 +1,9 @@
+﻿using ExtensionsLibrary;
+using System.Reactive.Linq;
+
+IObservable<string> weatherStationA = Observable.Throw<string>(new OutOfMemoryException());
+
+weatherStationA
+    .Log()
+    .Retry(3) // <-- three attempts to resubscribe to the source observable in case of an error
+    .SubscribeConsole("Retry");
