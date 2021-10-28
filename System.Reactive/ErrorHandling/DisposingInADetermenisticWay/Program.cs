1f944ccc76b57c034a61c2699837e1ed4f79d34d
ILepekhov <lepvanek@gmail.com>
2021-10-28 10:30:25 +0500

    Added an example of disposing of resources in a deterministic way (with the 'Using' operator).

---
+++ System.Reactive/ErrorHandling/DisposingInADetermenisticWay/Program.cs
@@ -0,0 +1,21 @@
+﻿using ExtensionsLibrary;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+Subject<int> subject = new();
+var observable = Observable.Using(() => Disposable.Create(() => { Console.WriteLine("DISPOSED"); }), _ => subject);
+
+Console.WriteLine("Disposed when completed");
+observable.SubscribeConsole();
+subject.OnCompleted();
+
+Console.WriteLine("Disposed when error occurs");
+subject = new();
+observable.SubscribeConsole();
+subject.OnError(new Exception("error"));
+
+Console.WriteLine("Disposed when subscription disposed");
+subject = new();
+var subscription = observable.SubscribeConsole();
+subscription.Dispose();
