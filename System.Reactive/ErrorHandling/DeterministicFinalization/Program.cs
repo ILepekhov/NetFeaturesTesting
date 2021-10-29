fb26e967b47c3a942fed821c7e680964c42d0284
ILepekhov <lepvanek@gmail.com>
2021-10-29 09:39:46 +0500

    Added an example of using the 'Finally' operator.

---
+++ System.Reactive/ErrorHandling/DeterministicFinalization/Program.cs
@@ -0,0 +1,27 @@
+﻿using ExtensionsLibrary;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+Console.WriteLine("Successful complete");
+Observable.Interval(TimeSpan.FromSeconds(1))
+    .Take(3)
+    .Finally(() => Console.WriteLine("Finally Code"))
+    .SubscribeConsole("Finally on Complete");
+
+Console.ReadLine();
+
+Console.WriteLine("Error termination");
+Observable.Throw<Exception>(new Exception("Error"))
+    .Finally(() => Console.WriteLine("Finnaly Code"))
+    .SubscribeConsole("Subscription on Error");
+
+Console.ReadLine();
+
+Console.WriteLine("Unsubscribing");
+Subject<int> subject = new();
+var subscription = subject.AsObservable()
+    .Finally(() => Console.WriteLine("Finally Code"))
+    .SubscribeConsole("Finally on observer disposing");
+subscription.Dispose();
+
+Console.ReadLine();
