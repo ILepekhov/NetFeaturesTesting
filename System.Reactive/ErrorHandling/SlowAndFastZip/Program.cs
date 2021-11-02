82dd7ef743bca2d59ff118f7b88b8d8b9edab68b
ILepekhov <lepvanek@gmail.com>
2021-11-02 08:23:54 +0500

    Added an example of the synthetic backpressure with the 'Zip' operator.

---
+++ System.Reactive/ErrorHandling/SlowAndFastZip/Program.cs
@@ -0,0 +1,10 @@
+﻿using ExtensionsLibrary;
+using System.Reactive.Linq;
+
+var fast = Observable.Interval(TimeSpan.FromSeconds(1)).Select(x => x * 10);
+var slow = Observable.Interval(TimeSpan.FromSeconds(2));
+
+slow.Zip(fast, (x, y) => x + y)
+    .SubscribeConsole("Zip");
+
+Console.ReadLine();
