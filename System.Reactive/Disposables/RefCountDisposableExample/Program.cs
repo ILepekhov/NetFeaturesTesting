a5bda0e8c1e41a6f53fd88a209cb5551fca92bc6
ILepekhov <lepvanek@gmail.com>
2021-11-08 10:08:21 +0500

    Added an example of using the RefCountDisposable

---
+++ System.Reactive/Disposables/RefCountDisposableExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reactive.Disposables;
+
+var inner = Disposable.Create(() => Console.WriteLine("Disposing inner-disposable"));
+
+var refCountDisposable = new RefCountDisposable(inner);
+
+var referenceOne = refCountDisposable.GetDisposable();
+var referenceTwo = refCountDisposable.GetDisposable();
+
+refCountDisposable.Dispose();
+
+Console.WriteLine("Disposing referenceOne");
+referenceOne.Dispose();
+
+Console.WriteLine("Disposing referenceTwo");
+referenceTwo.Dispose();
+
+Console.ReadLine();
