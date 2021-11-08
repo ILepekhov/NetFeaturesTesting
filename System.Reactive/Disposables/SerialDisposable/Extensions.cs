3452de7d6e65211ceb13a03d67501e47a4582c0b
ILepekhov <lepvanek@gmail.com>
2021-11-08 10:03:33 +0500

    Added an example of using the SerialDisposable instance

---
+++ System.Reactive/Disposables/SerialDisposable/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+namespace SerialDisposable
+{
+    internal static class Extensions
+    {
+        public static IObservable<TSource> MySubscribeOn<TSource>(this IObservable<TSource> source, IScheduler scheduler)
+        {
+            return Observable.Create<TSource>(observer =>
+            {
+                var d = new System.Reactive.Disposables.SerialDisposable();
+
+                d.Disposable = scheduler.Schedule(() =>
+                {
+                    Console.WriteLine("Dispose One");
+
+                    d.Disposable = new System.Reactive.Disposables.ScheduledDisposable(scheduler, System.Reactive.Disposables.Disposable.Create(() =>
+                    {
+                        Console.WriteLine("Dispose Two");
+                        source.SubscribeSafe(observer);
+                    }));
+                });
+
+                return d;
+            });
+        }
+    }
+}
---
+++ System.Reactive/Disposables/SerialDisposable/Program.cs
@@ -0,0 +1,14 @@
+﻿using ExtensionsLibrary;
+using SerialDisposable;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+IScheduler scheduler = ImmediateScheduler.Instance;
+
+var subscription = Observable.Timer(TimeSpan.FromSeconds(1))
+    .MySubscribeOn(scheduler)
+    .SubscribeConsole("SerialDisposable");
+
+subscription.Dispose();
+
+Console.ReadLine();
