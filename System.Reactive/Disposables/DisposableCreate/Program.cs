9f798c03766311bd73b711d49ab6cb2c3b585595
ILepekhov <lepvanek@gmail.com>
2021-11-08 09:42:13 +0500

    Added an example of creating a disposable with Disposable Rx library's Disposable.Create method

---
+++ System.Reactive/Disposables/DisposableCreate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Reactive.Disposables;
+
+PrintBusyState(true);
+
+using (Disposable.Create(() => PrintBusyState(false)))
+{
+    Console.WriteLine("Waiting for a long task");
+
+    await Task.Delay(2000);
+
+    Console.WriteLine("The long task finished");
+}
+
+Console.ReadLine();
+
+void PrintBusyState(bool busy)
+{
+    Console.WriteLine($"Is Busy = {busy}");
+}
