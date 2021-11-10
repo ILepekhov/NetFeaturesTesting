db46f7e298db4cf65650ec410ab30486b58e50e2
ILepekhov <lepvanek@gmail.com>
2021-11-10 10:07:52 +0500

    Added an example of the basic using of the TestScheduler.

---
+++ System.Reactive/Testing/TestSchedulerBaseUsing/Program.cs
@@ -0,0 +1,16 @@
+﻿using ExtensionsLibrary;
+using Microsoft.Reactive.Testing;
+using System.Reactive;
+
+var testScheduler = new TestScheduler();
+
+ITestableObservable<int> coldObservable = testScheduler.CreateColdObservable<int>(
+    new Recorded<Notification<int>>(20, Notification.CreateOnNext<int>(1)),
+    new Recorded<Notification<int>>(40, Notification.CreateOnNext<int>(2)),
+    new Recorded<Notification<int>>(60, Notification.CreateOnCompleted<int>()));
+
+coldObservable.SubscribeConsole("test");
+
+testScheduler.Start();
+
+Console.ReadLine();
