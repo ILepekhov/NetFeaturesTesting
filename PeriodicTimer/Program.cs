c5ea07002cfbe269e70411d68a9534ce0f44073d
ILepekhov <lepvanek@gmail.com>
2021-12-21 09:01:09 +0500

    Added an example of PeriodicalTimer using.

---
+++ PeriodicTimer/Program.cs
@@ -0,0 +1,21 @@
+﻿TimeSpan period = TimeSpan.FromSeconds(1);
+
+CancellationTokenSource cts = new(TimeSpan.FromMilliseconds(3500));
+
+DateTime startTime = DateTime.Now;
+
+try
+{
+    using PeriodicTimer timer = new(period);
+
+    while (await timer.WaitForNextTickAsync(cts.Token))
+    {
+        var timeDifference = (DateTime.Now - startTime).Milliseconds;
+
+        Console.WriteLine($"Offset: {timeDifference}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
