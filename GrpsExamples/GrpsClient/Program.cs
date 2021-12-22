d5ef071e7c4370d30fa7e2a032f110c851cb1b9c
ILepekhov <lepvanek@gmail.com>
2021-12-22 11:25:51 +0500

    Added GRPC communication example.

---
+++ GrpsExamples/GrpsClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpsClient;
+
+// We create a channel that represents the connection from client to the server
+// The URL that we add is provided from Kestrel in the server
+var channel = GrpcChannel.ForAddress("https://localhost:7042");
+
+// this the strongly typed client that was create for us from code generation
+// when we added the .proto file
+var greeterClient = new Greeter.GreeterClient(channel);
+
+var greeterResponse = await greeterClient.SayHelloAsync(new HelloRequest { Name = "Ivan" });
+
+Console.WriteLine($"The response from server: '{greeterResponse}'");
+
+var customerClient = new Customer.CustomerClient(channel);
+
+var customerResponse = await customerClient.GetCustomerInfoAsync(new CustomerFindModel { UserId = 1 });
+
+Console.WriteLine($"The response from server: First Name is '{customerResponse.FirstName}', Last Name is '{customerResponse.LastName}'");
+
+var customerCall = customerClient.GetAllCustomers(new Unit());
+
+Console.WriteLine("Making a stream request to CustomerService...");
+
+await foreach (var customer in customerCall.ResponseStream.ReadAllAsync())
+{
+    Console.WriteLine($"Customer: {customer}");
+}
+
+Console.ReadLine();
---
+++ GrpsExamples/GrpsServer/Services/CustomerService.cs
@@ -0,0 +1,74 @@
+﻿using Grpc.Core;
+
+namespace GrpsServer.Services;
+
+public sealed class CustomerService : Customer.CustomerBase
+{
+    private readonly ILogger<CustomerService> _logger;
+
+    public CustomerService(ILogger<CustomerService> logger)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public override Task<CustomerDataModel> GetCustomerInfo(CustomerFindModel request, ServerCallContext context)
+    {
+        CustomerDataModel result = new CustomerDataModel();
+
+        // This is a sample code for demo
+        // in real life scenarios this information should be fetched from the database
+        // no data should be hardcoded in the application
+        if (request.UserId == 1)
+        {
+            result.FirstName = "Mohamad";
+            result.LastName = "Lawand";
+        }
+        else if (request.UserId == 2)
+        {
+            result.FirstName = "Richard";
+            result.LastName = "Feynman";
+        }
+        else if (request.UserId == 3)
+        {
+            result.FirstName = "Bruce";
+            result.LastName = "Wayne";
+        }
+        else
+        {
+            result.FirstName = "James";
+            result.LastName = "Bond";
+        }
+
+        return Task.FromResult(result);
+    }
+
+    public override async Task GetAllCustomers(Unit request, IServerStreamWriter<CustomerDataModel> responseStream, ServerCallContext context)
+    {
+        var allCustomers = new List<CustomerDataModel>();
+
+        var c1 = new CustomerDataModel();
+        c1.FirstName = "Mohamad";
+        c1.LastName = "Lawand";
+        allCustomers.Add(c1);
+
+        var c2 = new CustomerDataModel();
+        c2.FirstName = "Richard";
+        c2.LastName = "Feynman";
+        allCustomers.Add(c2);
+
+        var c3 = new CustomerDataModel();
+        c3.FirstName = "Bruce";
+        c3.LastName = "Wayne";
+        allCustomers.Add(c3);
+
+        var c4 = new CustomerDataModel();
+        c4.FirstName = "James";
+        c4.LastName = "Bond";
+        allCustomers.Add(c4);
+
+        foreach (var item in allCustomers)
+        {
+            await responseStream.WriteAsync(item);
+        }
+    }
+}
