0e7ebc78968d36f662320929a58de4020d0081a1
ILepekhov <lepvanek@gmail.com>
2021-12-28 10:08:26 +0500

    Added an example of using the Expressions to get a property name.

---
+++ PropertyNameWithExpression/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq.Expressions;
+
+string GetMemberName<TSource, TMember>(TSource source, Expression<Func<TSource, TMember>> expression)
+{
+    if (expression is null) throw new ArgumentNullException(nameof(expression));
+
+    return GetMemberNamePrivate(expression.Body);
+}
+
+string GetMemberNamePrivate(Expression expression)
+{
+    if (expression is null) throw new ArgumentNullException(nameof(expression));
+
+    if (expression is MemberExpression memberExpression)
+        return memberExpression.Member.Name;
+
+    if (expression is MethodCallExpression methodCallExpression)
+        return methodCallExpression.Method.Name;
+
+    if (expression is UnaryExpression unaryExpression)
+        return GetMemberNamePrivate(unaryExpression.Operand);
+
+    throw new ArgumentException();
+}
+
+var instance = new SomeDummyClass();
+
+Console.WriteLine(GetMemberName(instance, x => x.Property1));
+Console.WriteLine(GetMemberName(instance, x => x.Property2));
+
+
+class SomeDummyClass
+{
+    public int Property1 { get; set; }
+
+    public int Property2 { get; set; }
+}
