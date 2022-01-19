78b9f0e798db302465d4fd690a453d5bfcb58de1
ILepekhov <lepvanek@gmail.com>
2022-01-19 10:51:26 +0500

    Added an example of "Modulo operation" algorithm

---
+++ OneCellSelectServerAlgoritm/Program.cs
@@ -0,0 +1,14 @@
+﻿var items = new List<string> { "one", "two", "three", "four", "five" };
+int itemsCount = items.Count;
+int pointer = 1;
+
+Parallel.For(0, 10, x =>
+{
+    var localPointer = Interlocked.Increment(ref pointer);
+
+    var itemIndex = localPointer % itemsCount;
+
+    var item = items[itemIndex];
+
+    Console.WriteLine($"x = {x}, localPointer = {localPointer}, itemIndex = {itemIndex}, item = {item}");
+});
