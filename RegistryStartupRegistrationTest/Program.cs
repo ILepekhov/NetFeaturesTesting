3695cf6796e332e158fe3efba132705398009e24
ILepekhov <lepvanek@gmail.com>
2022-01-24 12:47:17 +0500

    Added an example of registration application for Startup on Windows Start.

---
+++ RegistryStartupRegistrationTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using RegistryStartupRegistrationTest;
+using System.Reflection;
+
+string appTitle = "RegistryStartupRegistration Test";
+string path = Assembly.GetCallingAssembly().Location;
+
+path = Path.ChangeExtension(path, "exe");
+
+bool isInStartup = Startup.IsInStartup(appTitle);
+
+if (isInStartup)
+{
+    Console.WriteLine("Application is in Startup");
+
+    bool unregistered = Startup.RemoveFromStartup(appTitle);
+
+    if (unregistered)
+        Console.WriteLine("Application unregistered from Startup");
+    else
+        Console.WriteLine("Failed to unregister Application");
+}
+else
+{
+    Console.WriteLine("Application is not in Startup");
+
+    bool registered = Startup.RunOnStartup(appTitle, path);
+
+    if (registered)
+        Console.WriteLine("Application registered in Startup");
+    else
+        Console.WriteLine("Failed to register Application in Startup");
+}
+
+
---
+++ RegistryStartupRegistrationTest/Startup.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+
+namespace RegistryStartupRegistrationTest;
+
+internal static class Startup
+{
+    public static bool RunOnStartup(string appTitle, string appPath)
+    {
+        try
+        {
+            RegistryKey? registryKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+
+            if (registryKey is null)
+                return false;
+
+            registryKey.SetValue(appTitle, appPath);
+
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public static bool RemoveFromStartup(string appTitle)
+    {
+        try
+        {
+            RegistryKey? registryKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+
+            if (registryKey is null)
+                return false;
+
+            registryKey.DeleteValue(appTitle);
+
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public static bool IsInStartup(string appTitle)
+    {
+        try
+        {
+            RegistryKey? registryKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+
+            if (registryKey is null)
+                return false;
+
+            var value = registryKey.GetValue(appTitle);
+
+            return value is not null;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+}
