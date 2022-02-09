eed430711717944e0a6251a8663ac08f2b3ab2b2
ILepekhov <lepvanek@gmail.com>
2022-02-09 23:45:06 +0500

    An example of using the Barrer threading object has been added.

---
+++ BarrierTest/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] results = new string[2];
+
+Barrier barrier = new Barrier(2, b =>
+{
+    Console.WriteLine("Results: " + string.Join(", ", results));
+});
+
+var task1 = Task.Run(() =>
+{
+    results[0] = "Thread 1 id: " + Environment.CurrentManagedThreadId;
+
+    barrier.SignalAndWait();
+});
+
+Console.WriteLine("Sleeping for 1 sec");
+
+Thread.Sleep(1000);
+
+var task2 = Task.Run(() =>
+{
+    results[1] = "Thread 2 id: " + Environment.CurrentManagedThreadId;
+
+    barrier.SignalAndWait();
+});
+
+Task.WaitAll(task1, task2);
+
