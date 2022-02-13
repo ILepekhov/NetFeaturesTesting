2b3c0b6f583a65d328d1c004f4ea566cf7fc4849
ILepekhov <lepvanek@gmail.com>
2022-02-13 22:39:46 +0500

    An example of the Binary Search algorithm added

---
+++ GrokkingAlgorithms/BinarySearch/Program.cs
@@ -0,0 +1,27 @@
+﻿int ExecuteBinarySearch(int[] array, int item)
+{
+    var low = 0;
+    var high = array.Length - 1;
+
+    while (low <= high)
+    {
+        var mid = (low + high) / 2;
+
+        var guess = array[mid];
+
+        if (guess == item)
+            return mid;
+
+        if (guess > item)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+
+    return -1;
+}
+
+int[] array = Enumerable.Range(1, 100).ToArray();
+
+Console.WriteLine(ExecuteBinarySearch(array, 3));
+Console.WriteLine(ExecuteBinarySearch(array, -10));
